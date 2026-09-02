083dce49d79d2cd14aa88797b874d43b8770c8ef
S.Nazari <sajjad69nazari@gmail.com>
2026-09-02 16:23:08 +0330

    Add Project Layers & Architecture

---
+++ src/Blog/Presentation/Blog.Api/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is working!");
+        }
+    }
+}
