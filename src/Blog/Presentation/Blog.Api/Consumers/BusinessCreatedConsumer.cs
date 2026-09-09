d53a752350f940fcc57ed1a1031fde4c0a1b2013
S.Nazari <sajjad69nazari@gmail.com>
2026-09-09 17:26:32 +0330

    Add MassTransit.RabbitMQ & BusinessCreatedConsumer & UsingRabbitMq Config

---
+++ src/Blog/Presentation/Blog.Api/Consumers/BusinessCreatedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Application.Features.Articles.Commands.CreateArticle;
+using Contracts.Events;
+using MassTransit;
+using Mediator;
+
+namespace Blog.Api.Consumers
+{
+    public class BusinessCreatedConsumer(IMediator mediator) : IConsumer<BusinessCreatedEvent>
+    {
+        public async Task Consume(ConsumeContext<BusinessCreatedEvent> context)
+        {
+            // تبدیل رویداد بیرونی به یک دستور داخلی (Command)
+            var command = new CreateArticleCommand(
+                context.Message.BusinessId,
+                $"به بلاگ {context.Message.BusinessTitle} خوش آمدید!",
+                "این اولین پست بلاگ شماست."
+            );
+
+            // ارسال به لایه Application توسط Mediator
+            await mediator.Send(command, context.CancellationToken);
+        }
+    }
+}
--- src/Blog/Presentation/Blog.Api/Program.cs
+++ src/Blog/Presentation/Blog.Api/Program.cs
@@ -1,4 +1,6 @@
-﻿using Blog.Infrastructure;
+﻿using Blog.Api.Consumers;
+using Blog.Infrastructure;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,24 @@ builder.Services.AddMediator(options =>
 {
     options.ServiceLifetime = ServiceLifetime.Scoped;
 });
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<BusinessCreatedConsumer>(); // معرفی گیرنده
 
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h => {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        // ساخت یک صف اختصاصی برای بلاگ
+        cfg.ReceiveEndpoint("blog-business-created-queue", e =>
+        {
+            e.ConfigureConsumer<BusinessCreatedConsumer>(context);
+        });
+    });
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
